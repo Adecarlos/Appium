f95c728e5e4c43977d1b025dee44ca4cd9feb7a8
Adecarlos <60363276+Adecarlos@users.noreply.github.com>
2024-05-01 15:29:49 -0300

    Appium server Programmatically working
    Depois de ter problemas com o Appium rodando local no eclipse e globalmente no Prompt de comando. Após arrumar isso tive que achar o arquivo main.js correto

--- src/test/java/AutomatizadoMobile/Appium/AppiumBasics.java
+++ src/test/java/AutomatizadoMobile/Appium/AppiumBasics.java
@@ -1,5 +1,6 @@
 package AutomatizadoMobile.Appium;
 
+import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URISyntaxException;
@@ -11,14 +12,21 @@ import org.testng.annotations.Test;
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.options.UiAutomator2Options;
+import io.appium.java_client.service.local.AppiumDriverLocalService;
+import io.appium.java_client.service.local.AppiumServiceBuilder;
 
 
 public class AppiumBasics {
 
 	@Test
 	public void AppiumTest() throws MalformedURLException, URISyntaxException {
-	
-	
+		
+	AppiumDriverLocalService service = new AppiumServiceBuilder().withAppiumJS(new File("C:\\Users\\jwest\\AppData\\Roaming\\npm\\node_modules\\appium\\build\\lib\\main.js"))		
+			
+			.withIPAddress("127.0.0.1")
+			.usingPort(4723)
+			.build();
+	service.start();
 	UiAutomator2Options options = new UiAutomator2Options();
 	
 	
@@ -30,5 +38,6 @@ public class AppiumBasics {
 	
 	AndroidDriver driver = new AndroidDriver(new URI("http://127.0.0.1:4723").toURL(),options);
 	driver.quit();
+	service.stop();
 	}
 }
