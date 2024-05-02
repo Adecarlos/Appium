7d44f492ec799af9e405938358447815a76843d8
Adecarlos <60363276+Adecarlos@users.noreply.github.com>
2024-05-02 01:09:31 -0300

    Usar 4 tipos de forma de buscar elementos

--- src/test/java/AutomatizadoMobile/Appium/AppiumBasics.java
+++ src/test/java/AutomatizadoMobile/Appium/AppiumBasics.java
@@ -1,20 +1,12 @@
 package AutomatizadoMobile.Appium;
 
-import java.io.File;
 import java.net.MalformedURLException;
-import java.net.URI;
 import java.net.URISyntaxException;
-import java.net.URL;
 
-import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.By;
 import org.testng.annotations.Test;
 
 import io.appium.java_client.AppiumBy;
-import io.appium.java_client.AppiumDriver;
-import io.appium.java_client.android.AndroidDriver;
-import io.appium.java_client.android.options.UiAutomator2Options;
-import io.appium.java_client.service.local.AppiumDriverLocalService;
-import io.appium.java_client.service.local.AppiumServiceBuilder;
 
 
 public class AppiumBasics extends BaseTest {
@@ -24,7 +16,9 @@ public class AppiumBasics extends BaseTest {
 
 	//Automação
 	driver.findElement(AppiumBy.accessibilityId("Preference")).click();
-	
-	
+	driver.findElement(By.xpath("//android.widget.TextView[@content-desc='3. Preference dependencies']")).click();
+	driver.findElement(By.id("android:id/checkbox")).click();
+	driver.findElement(By.xpath("(//android.widget.RelativeLayout)[2]")).click();
+
 	}
 }
