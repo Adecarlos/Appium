41fcbf45082a890a0edca66bdcc166945117bc3b
Adecarlos <60363276+Adecarlos@users.noreply.github.com>
2024-05-05 12:27:41 -0300

    Adicionado AssertEquals e implicityWait

--- src/test/java/AutomatizadoMobile/Appium/AppiumBasics.java
+++ src/test/java/AutomatizadoMobile/Appium/AppiumBasics.java
@@ -4,6 +4,7 @@ import java.net.MalformedURLException;
 import java.net.URISyntaxException;
 
 import org.openqa.selenium.By;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import io.appium.java_client.AppiumBy;
@@ -19,6 +20,10 @@ public class AppiumBasics extends BaseTest {
 	driver.findElement(By.xpath("//android.widget.TextView[@content-desc='3. Preference dependencies']")).click();
 	driver.findElement(By.id("android:id/checkbox")).click();
 	driver.findElement(By.xpath("(//android.widget.RelativeLayout)[2]")).click();
+	String AlertText = driver.findElement(By.id("android:id/alertTitle")).getText();
+	Assert.assertEquals(AlertText, "WiFi settings");
+	driver.findElement(By.id("android:id/edit")).sendKeys("teste");
+	driver.findElement(By.id("android:id/button1")).click();
 
 	}
 }
--- src/test/java/AutomatizadoMobile/Appium/BaseTest.java
+++ src/test/java/AutomatizadoMobile/Appium/BaseTest.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.time.Duration;
 
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
@@ -35,6 +36,7 @@ public class BaseTest {
 				"D:\\Arquivos de Programas\\Eclipse\\eclipse-workspace\\Appium\\src\\test\\java\\Resources\\ApiDemos-debug.apk");
 
 		driver = new AndroidDriver(new URI("http://127.0.0.1:4723").toURL(), options);
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
 	}
 	
